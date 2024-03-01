7ccee3098e93d73e1666a4e029b0809339299b72
Herman Lundby-Holen <hermanluho@gmail.com>
2024-03-01 15:04:00 +0100

    Stylized the bottom pane.

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/CardGameUi.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/CardGameUi.java
@@ -6,6 +6,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -91,19 +92,96 @@ public class CardGameUi extends Application {
    *
    * @return The bottom pane.
    */
-  private HBox createBottomPane() {
-    // creates a horizontal box which holds sum of the faces
-    HBox sumPane = new HBox();
-    Text sumText = new Text("Sum of faces: ");
-    Text sumValue = new Text("0"); // TODO: Bind this to the actual sum of the faces dealt
-    sumPane.getChildren().addAll(sumText, sumValue);
+  private FlowPane createBottomPane() {
+    // creates a sum pane
+    HBox sumPane = this.createSumPane();
+    // creates a heart pane
+    HBox heartPane = this.createHeartPane();
+    // creates a flush pane
+    HBox flushPane = this.createFlushPane();
+    // creates a queen of spades pane
+    HBox queenOfSpadesPane = this.createQueenOfSpadesPane();
 
     // creates a horizontal box
-    HBox bottomPane = new HBox();
-    // adds the sum pane to the horizontal box
-    bottomPane.getChildren().add(sumPane);
+    FlowPane bottomPane = new FlowPane();
+    // adds all the bottom panes to the horizontal box
+    bottomPane.getChildren().addAll(sumPane, heartPane, flushPane, queenOfSpadesPane);
+    // sets the spacing between the panes
+    bottomPane.setHgap(20);
 
     return bottomPane;
+  }
+
+  /**
+   * Displays the sum of the faces of the cards in the hand.
+   * TODO: Bind this to the actual sum of the faces dealt
+   *
+   * @return The sum of the faces of the cards in the hand.
+   */
+  private HBox createSumPane() {
+    HBox sumPane = new HBox();
+
+    Text sumText = new Text("Sum of faces: ");
+    Text sumValue = new Text("0");
+
+    sumPane.getChildren().addAll(sumText, sumValue);
+    sumPane.getStyleClass().add("bottom-pane-child");
+
+    return sumPane;
+  }
+
+  /**
+   * Displays only the cards in the hand that are of the suit hearts.
+   * TODO: Replace this with a pane that displays the heart-card in the hand.
+   *
+   * @return The pane that displays the heart-cards in the hand.
+   */
+  private HBox createHeartPane() {
+    HBox heartPane = new HBox();
+
+    Text heartText = new Text("Cards of hearts: ");
+    Text heartCards = new Text("H12, H1");
+
+    heartPane.getChildren().addAll(heartText, heartCards);
+    heartPane.getStyleClass().add("bottom-pane-child");
+
+    return heartPane;
+  }
+
+  /**
+   * Displays whether the hand is a flush or not.
+   * TODO: Bind this to the actual hand dealt.
+   *
+   * @return The pane that displays whether the hand is a flush or not.
+   */
+  private HBox createFlushPane() {
+    HBox flushPane = new HBox();
+
+    Text flushText = new Text("Flush: ");
+    Text flushValue = new Text("Yes/No");
+
+    flushPane.getChildren().addAll(flushText, flushValue);
+    flushPane.getStyleClass().add("bottom-pane-child");
+
+    return flushPane;
+  }
+
+  /**
+   * Displays whether the hand contains the queen of spades or not.
+   * TODO: Bind this to the actual hand dealt.
+   *
+   * @return The pane that displays whether the hand contains the queen of spades or not.
+   */
+  private HBox createQueenOfSpadesPane() {
+    HBox queenOfSpadesPane = new HBox();
+
+    Text queenOfSpadesText = new Text("Queen of spades: ");
+    Text queenOfSpadesValue = new Text("Yes/No");
+
+    queenOfSpadesPane.getChildren().addAll(queenOfSpadesText, queenOfSpadesValue);
+    queenOfSpadesPane.getStyleClass().add("bottom-pane-child");
+
+    return queenOfSpadesPane;
   }
 
   /**
